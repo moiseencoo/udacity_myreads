0e96eed630ddc37a7d0e76593401a0a35667a29d
Varvara Moiseenko <95varya@mail.ru>
2020-03-11 03:01:05 +0500

    feat: create SearchBooks component

---
+++ src/components/SearchBooks.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class SearchBooks extends React.Component {
+    render() {
+        return(
+            <div className="open-search">
+            <button>
+              Add a book
+            </button>
+          </div>
+        )
+    }
+}
+
+export default SearchBooks

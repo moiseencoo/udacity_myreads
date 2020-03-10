fe84359ac078e1ae9ada830d645456e1bf4e0423
Varvara Moiseenko <95varya@mail.ru>
2020-03-11 03:30:02 +0500

    feat: add API to SearchBooks component

--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import * as BooksAPI from '../BooksAPI';
 
 class SearchBooks extends React.Component {
+    state = {
+        books: [],
+        query: ''
+    }
+
+    search(query) {
+        BooksAPI.search(query)
+        .then(books => {
+            this.setState({ books })
+        })
+    }
+
     render() {
         return(
-            <div className="open-search">
-            <button>
-              Add a book
-            </button>
+            <div className="search-books">
+            <div className="search-books-bar">
+              <button
+                className="close-search"
+                onClick={() => this.setState({ showSearchPage: false })}
+              >
+                Close
+              </button>
+              <div className="search-books-input-wrapper">
+                <input type="text" placeholder="Search by title or author" />
+              </div>
+            </div>
+            <div className="search-books-results">
+              <ol className="books-grid"></ol>
+            </div>
           </div>
         )
     }

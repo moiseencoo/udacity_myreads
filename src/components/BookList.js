32f4bde0b6eab4d90ca778e3d68859ac65bbd8e9
Varvara Moiseenko <95varya@mail.ru>
2020-03-11 03:56:11 +0500

    feat: add router link to search

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,43 +1,46 @@
 import React from "react";
 import Shelf from "./Shelf";
+import { Link } from 'react-router-dom'
 
 class BookList extends React.Component {
   render() {
-
     const { books } = this.props;
 
     const shelves = [
-        {
-            name: "Currently Reading",
-            slug: "currentlyReading"
-        },
-        {
-            name: "Want To Read",
-            slug: "wantToRead"
-        },
-        {
-            name: "Read",
-            slug: "read"
-        }
-    ]
+      {
+        name: "Currently Reading",
+        slug: "currentlyReading"
+      },
+      {
+        name: "Want To Read",
+        slug: "wantToRead"
+      },
+      {
+        name: "Read",
+        slug: "read"
+      }
+    ];
     return (
-      <React.Fragment>
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
         <div className="list-books-content">
           <div>
-            {
-                shelves.map((shelf) => {
-                    let filtered_books = books.filter((book) => book.shelf === shelf.slug)
-                    return (
-                        <Shelf shelfTitle={shelf.name} books={filtered_books} />
-                    )
-                })
-            }
+            {shelves.map(shelf => {
+              let filtered_books = books.filter(
+                book => book.shelf === shelf.slug
+              );
+              return <Shelf shelfTitle={shelf.name} books={filtered_books} />;
+            })}
           </div>
         </div>
         <div className="open-search">
-          <button>Add a book</button>
+          <Link to="/search" className="btn-add">
+            Add a book
+          </Link>
         </div>
-      </React.Fragment>
+      </div>
     );
   }
 }
